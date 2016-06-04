21f80963ac4dabdf732a34bc0d89603d1123fcfd
Barry Byford <barry_byford@yahoo.co.uk>
2016-06-04 20:12:37 +0100

    Working charting (#2)
    * Initial commit
    
    * Tidy up
    
    * Chart characteristic added
    
    * Add favicon
    
    * sensor value
    
    * Value being displayed
    
    * bar chart drawing
    
    * Improved graphing

---
+++ chart/getCharacteristic.js
@@ -0,0 +1,83 @@
+
+(function() {
+  'use strict';
+
+  class ReadSensor {
+    constructor() {
+      this.device = null;
+      this.server = null;
+      this._characteristics = new Map();
+    }
+    connect() {
+      return navigator.bluetooth.requestDevice({filters:[{services:[ '11118000-2222-3333-4444-555566667777' ]}]})
+      .then(device => {
+        this.device = device;
+        return device.gatt.connect();
+      })
+      .then(server => {
+        this.server = server;
+        return Promise.all([
+          server.getPrimaryService('11118000-2222-3333-4444-555566667777').then(service => {
+            return Promise.all([
+              this._cacheCharacteristic(service, '11118010-2222-3333-4444-555566667777'),
+            ]);
+          })
+        ]);
+      });
+    }
+    startNotificationsSensorMeasurement() {
+      return this._startNotifications('11118010-2222-3333-4444-555566667777');
+    }
+
+    stopNotificationsSensorMeasurement() {
+      return this._stopNotifications('11118010-2222-3333-4444-555566667777');
+    }
+
+    parseSensor(value) {
+      // In Chrome 50+, a DataView is returned instead of an ArrayBuffer.
+      let result = {};
+      value = value.buffer ? value : new DataView(value);
+      result.sensor = value.getUint16(0, /*littleEndian=*/true);
+      return result;
+    }
+
+    /* Utils */
+
+    _cacheCharacteristic(service, characteristicUuid) {
+      return service.getCharacteristic(characteristicUuid)
+      .then(characteristic => {
+        this._characteristics.set(characteristicUuid, characteristic);
+      });
+    }
+    _readCharacteristicValue(characteristicUuid) {
+      let characteristic = this._characteristics.get(characteristicUuid);
+      return characteristic.readValue()
+      .then(value => {
+        // In Chrome 50+, a DataView is returned instead of an ArrayBuffer.
+        value = value.buffer ? value : new DataView(value);
+        return value;
+      });
+    }
+    _writeCharacteristicValue(characteristicUuid, value) {
+      let characteristic = this._characteristics.get(characteristicUuid);
+      return characteristic.writeValue(value);
+    }
+    _startNotifications(characteristicUuid) {
+      let characteristic = this._characteristics.get(characteristicUuid);
+      // Returns characteristic to set up characteristicvaluechanged event
+      // handlers in the resolved promise.
+      return characteristic.startNotifications()
+      .then(() => characteristic);
+    }
+    _stopNotifications(characteristicUuid) {
+      let characteristic = this._characteristics.get(characteristicUuid);
+      // Returns characteristic to remove characteristicvaluechanged event
+      // handlers in the resolved promise.
+      return characteristic.stopNotifications()
+      .then(() => characteristic);
+    }
+  }
+
+  window.readSensor = new ReadSensor();
+
+})();
